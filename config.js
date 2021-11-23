9c4f71819fdfc231a03679f23ea6861c9a7d1008
HuyyNek <sautimthiephong4319@gmail.com>
2021-11-23 21:10:24 +0700

    upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ú òa :D!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Ừm hứm :3',
+    title: 'Cậu thích tớ rồi đúng không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu đã yêu tớ rồi đóa :> ',
+    btnYes: 'Yêu chứ <3 <3 <3',
+    btnNo: 'Hờ, mơ đi nhaaa',
+    question:'Seo lại iu tớ tkee :> <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì Huy thông minh và hiểu tớ <3',
+    mess: 'Biết ngay mờ 🥰. Love you 300.000 😘😘',
+    messDesc: 'ib với mềnh luôn nhớ ;)',
+    btnAccept: 'Okiiiii cậu <3',
+    messLink: 'https://www.facebook.com/huy.le.315428/'
+}
